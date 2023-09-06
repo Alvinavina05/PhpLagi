a3b6fc278f9a133062d83c0711b2c9bcd0911b44
Alvinavina05 <alvinasugiarti@gmail.com>
2023-09-06 12:42:36 +0700

    belajar

---
+++ CobaPhp.php
@@ -0,0 +1,35 @@
+<?php
+$nama="Alvina";
+$alamat="Tapen";
+$nim="E41220499";
+$nohp="0565676456";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <table border=1>
+    <tr>
+        <td>Nama</td>
+        <td>Alamat</td>
+        <td>Nim</td>
+        <td>NoHp</td>
+
+        <tr>
+        <td><?php echo $nama; ?></td>
+        <td><?php echo $alamat; ?></td>
+        <td><?php echo $nim; ?></td>
+        <td><?php echo $nohp; ?></td>
+
+        </tr>
+
+    </tr>
+</body>
+</html>
